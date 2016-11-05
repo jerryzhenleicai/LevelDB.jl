a2395c132a438e73d67ff70d27801cc6c6a578e1
Jerry ChanjetJerry <jerry.cai@us.chanjet.com>
2016-11-04 18:24:54 -0700

    BinDeps script does not work with Julia 0.5, undo bindeps related changes

--- src/LevelDB.jl
+++ src/LevelDB.jl
@@ -1,13 +1,5 @@
 module LevelDB
 
-using BinDeps
-
-depsfile = Pkg.dir("LevelDB","deps","deps.jl")
-if isfile(depsfile)
-    include(depsfile)
-else
-    error("LevelDB not properly installed. Please run Pkg.build(\"LevelDB\")")
-end
 
 export open_db
 export close_db
@@ -22,13 +14,13 @@ export range_close
 
 
 function open_db(file_path, create_if_missing)
-    options = ccall( (:leveldb_options_create, libleveldbjl), Ptr{Void}, ())
+    options = ccall( (:leveldb_options_create, "libleveldb"), Ptr{Void}, ())
     if create_if_missing
-        ccall( (:leveldb_options_set_create_if_missing, libleveldbjl), Void,
+        ccall( (:leveldb_options_set_create_if_missing, "libleveldb"), Void,
               (Ptr{Void}, UInt8), options, 1)
     end
     err = Ptr{UInt8}[0]
-    db = ccall( (:leveldb_open, libleveldbjl), Ptr{Void},
+    db = ccall( (:leveldb_open, "libleveldb"), Ptr{Void},
                (Ptr{Void}, Ptr{UInt8}, Ptr{Ptr{UInt8}}) , options, file_path, err)
 
     if db == C_NULL
@@ -39,13 +31,13 @@ end
 
 
 function close_db(db)
-    ccall( (:leveldb_close, libleveldbjl), Void, (Ptr{Void},), db)
+    ccall( (:leveldb_close, "libleveldb"), Void, (Ptr{Void},), db)
 end
 
 function db_put(db, key, value, val_len)
-    options = ccall( (:leveldb_writeoptions_create, libleveldbjl), Ptr{Void}, ())
+    options = ccall( (:leveldb_writeoptions_create, "libleveldb"), Ptr{Void}, ())
     err = Ptr{UInt8}[0]
-    ccall( (:leveldb_put, libleveldbjl), Void,
+    ccall( (:leveldb_put, "libleveldb"), Void,
           (Ptr{Void}, Ptr{Void}, Ptr{UInt8}, UInt, Ptr{UInt8}, UInt, Ptr{Ptr{UInt8}} ),
           db, options,key, length(key), value, val_len, err)
     if err[1] != C_NULL
@@ -56,10 +48,10 @@ end
 # return an UInt8 array obj
 function db_get(db, key)
     # leveldb_get will allocate the buffer for return value
-    options = ccall( (:leveldb_readoptions_create, libleveldbjl), Ptr{Void}, ())
+    options = ccall( (:leveldb_readoptions_create, "libleveldb"), Ptr{Void}, ())
     err = Ptr{UInt8}[0]
     val_len = Csize_t[0]
-    value = ccall( (:leveldb_get, libleveldbjl), Ptr{UInt8},
+    value = ccall( (:leveldb_get, "libleveldb"), Ptr{UInt8},
           (Ptr{Void}, Ptr{Void}, Ptr{UInt8}, UInt, Ptr{Csize_t},  Ptr{Ptr{UInt8}} ),
           db, options, key, length(key), val_len, err)
     if err[1] != C_NULL
@@ -71,9 +63,9 @@ function db_get(db, key)
 end
 
 function db_delete(db, key)
-    options = ccall( (:leveldb_writeoptions_create, libleveldbjl), Ptr{Void}, ())
+    options = ccall( (:leveldb_writeoptions_create, "libleveldb"), Ptr{Void}, ())
     err = Ptr{UInt8}[0]
-    ccall( (:leveldb_delete, libleveldbjl), Void,
+    ccall( (:leveldb_delete, "libleveldb"), Void,
           (Ptr{Void}, Ptr{Void}, Ptr{UInt8}, UInt, Ptr{Ptr{UInt8}} ),
           db, options, key, length(key), err)
     if err[1] != C_NULL
@@ -83,22 +75,22 @@ end
 
 
 function create_write_batch()
-    batch = ccall( (:leveldb_writebatch_create, libleveldbjl), Ptr{Void},())
+    batch = ccall( (:leveldb_writebatch_create, "libleveldb"), Ptr{Void},())
     return batch
 end
 
 
 
 function batch_put(batch, key, value, val_len)
-    ccall( (:leveldb_writebatch_put, libleveldbjl), Void,
+    ccall( (:leveldb_writebatch_put, "libleveldb"), Void,
           (Ptr{UInt8}, Ptr{UInt8}, UInt, Ptr{UInt8}, UInt),
           batch, key, length(key), value, val_len)
 end
 
 function write_batch(db, batch)
-    options = ccall( (:leveldb_writeoptions_create, libleveldbjl), Ptr{Void}, ())
+    options = ccall( (:leveldb_writeoptions_create, "libleveldb"), Ptr{Void}, ())
     err = Ptr{UInt8}[0]
-    ccall( (:leveldb_write, libleveldbjl), Void,
+    ccall( (:leveldb_write, "libleveldb"), Void,
           (Ptr{Void}, Ptr{Void}, Ptr{Void},  Ptr{Ptr{UInt8}} ),
           db, options, batch, err)
     if err[1] != C_NULL
@@ -109,20 +101,20 @@ end
 
 
 function create_iter(db::Ptr{Void}, options::Ptr{Void})
-  ccall( (:leveldb_create_iterator, libleveldbjl), Ptr{Void},
+  ccall( (:leveldb_create_iterator, "libleveldb"), Ptr{Void},
               (Ptr{Void}, Ptr{Void}),
               db, options)
 end
 
 function iter_valid(it::Ptr{Void})
-  ccall( (:leveldb_iter_valid, libleveldbjl), UInt8,
+  ccall( (:leveldb_iter_valid, "libleveldb"), UInt8,
     (Ptr{Void},),
     it) == 1
 end
 
 function iter_key(it::Ptr{Void})
   k_len = Csize_t[0]
-  key = ccall( (:leveldb_iter_key, libleveldbjl), Ptr{UInt8},
+  key = ccall( (:leveldb_iter_key, "libleveldb"), Ptr{UInt8},
     (Ptr{Void}, Ptr{Csize_t}),
     it, k_len)
   bytestring(key, k_len[1])
@@ -130,20 +122,20 @@ end
 
 function iter_value(it::Ptr{Void})
   v_len = Csize_t[0]
-  value = ccall( (:leveldb_iter_value, libleveldbjl), Ptr{UInt8},
+  value = ccall( (:leveldb_iter_value, "libleveldb"), Ptr{UInt8},
     (Ptr{Void}, Ptr{Csize_t}),
     it, v_len)
   pointer_to_array(value, (v_len[1],), false)
 end
 
 function iter_seek(it::Ptr{Void}, key)
-  ccall( (:leveldb_iter_seek, libleveldbjl), Void,
+  ccall( (:leveldb_iter_seek, "libleveldb"), Void,
     (Ptr{Void}, Ptr{UInt8}, UInt),
     it, key, length(key))
 end
 
 function iter_next(it::Ptr{Void})
-  ccall( (:leveldb_iter_next, libleveldbjl), Void,
+  ccall( (:leveldb_iter_next, "libleveldb"), Void,
     (Ptr{Void},),
     it)
 end
@@ -157,7 +149,7 @@ type Range
 end
 
 function db_range(db, key_start, key_end="\uffff")
-  options = ccall( (:leveldb_readoptions_create, libleveldbjl), Ptr{Void}, ())
+  options = ccall( (:leveldb_readoptions_create, "libleveldb"), Ptr{Void}, ())
   iter = create_iter(db, options)
   Range(iter, options, key_start, key_end, false)
 end
@@ -165,10 +157,10 @@ end
 function range_close(range::Range)
   if !range.destroyed
     range.destroyed = true
-    ccall( (:leveldb_iter_destroy, libleveldbjl), Void,
+    ccall( (:leveldb_iter_destroy, "libleveldb"), Void,
       (Ptr{Void},),
       range.iter)
-    ccall( (:leveldb_readoptions_destroy, libleveldbjl), Void,
+    ccall( (:leveldb_readoptions_destroy, "libleveldb"), Void,
       (Ptr{Void},),
       range.options)
   end
